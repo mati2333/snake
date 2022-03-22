fb7f803ef2d592bbb764fee1d31e9c506fbda8ea
mati2333 <82666561+mati2333@users.noreply.github.com>
2022-03-22 19:33:10 +0100

    Add files via upload

---
+++ Board.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.awt.*;
+
+public class Board {
+    public static final int FIELD_X = 35;
+    public static final int FIELD_Y = 30;
+    public static final int SIZE = 25;
+    public static final int MAX_X = FIELD_X*SIZE;
+    public static final int MAX_Y = FIELD_Y*SIZE;
+
+    public static void draw(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0, MAX_X, MAX_Y);
+    }
+}
---
+++ Game.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.awt.*;
+
+public class Game {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            MainFrame mainFrame = new MainFrame();
+            mainFrame.setVisible(true);
+        });
+    }
+}
